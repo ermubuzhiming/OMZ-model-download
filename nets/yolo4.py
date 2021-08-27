4c72236c6fb5829ac480d316ed320d20fd14e9fd
Lin,Ying <ying.lin@intel.com>
2021-08-27 20:23:36 +0800

    Update yolo4.py

--- nets/yolo4.py
+++ nets/yolo4.py
@@ -11,7 +11,7 @@ from utils.utils import compose
 
 
 # --------------------------------------------------#
-#   单次卷积
+#   A single convolution
 # --------------------------------------------------#
 @wraps(Conv2D)
 @wraps(SeparableConv2D)
@@ -23,7 +23,7 @@ def DarknetConv2D(*args, **kwargs):
 
 
 # ---------------------------------------------------#
-#   卷积块
+#   
 #   DarknetConv2D + BatchNormalization + LeakyReLU
 # ---------------------------------------------------#
 def DarknetConv2D_BN_Leaky(*args, **kwargs):
@@ -36,10 +36,10 @@ def DarknetConv2D_BN_Leaky(*args, **kwargs):
 
 
 # ---------------------------------------------------#
-#   特征层->最后的输出
+#   Characteristics of layer
 # ---------------------------------------------------#
 def make_five_convs(x, num_filters):
-    # 五次卷积
+    # Five times convolution
     x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
     x = DarknetConv2D_BN_Leaky(num_filters * 2, (3, 3))(x)
     x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
@@ -49,22 +49,22 @@ def make_five_convs(x, num_filters):
 
 
 # ---------------------------------------------------#
-#   特征层->最后的输出
+#   Characteristics of layer
 # ---------------------------------------------------#
 def yolo_body(inputs, num_anchors, num_classes):
-    # 生成darknet53的主干模型
+    # Build the trunk model for darknet53
     mobilenetv2 = MobileNetV2(input_tensor=inputs, weights=None)
     feat3 = mobilenetv2.get_layer('block_16_project_BN').output
     feat2 = mobilenetv2.get_layer('block_12_project_BN').output
     feat1 = mobilenetv2.get_layer('block_5_project_BN').output
     # feat1,feat2,feat3 = darknet_body(inputs)
 
-    # 第一个特征层
+    # The first feature layer
     # y1=(batch_size,13,13,3,85)
     p5 = DarknetConv2D_BN_Leaky(512, (1, 1))(feat3)
     p5 = DarknetConv2D_BN_Leaky(1024, (3, 3))(p5)
     p5 = DarknetConv2D_BN_Leaky(512, (1, 1))(p5)
-    # 使用了SPP结构，即不同尺度的最大池化后堆叠。
+    # Use the SPP structure
     maxpool1 = MaxPooling2D(pool_size=(13, 13), strides=(1, 1), padding='same')(p5)
     maxpool2 = MaxPooling2D(pool_size=(9, 9), strides=(1, 1), padding='same')(p5)
     maxpool3 = MaxPooling2D(pool_size=(5, 5), strides=(1, 1), padding='same')(p5)
@@ -110,14 +110,14 @@ def yolo_body(inputs, num_anchors, num_classes):
 
 
 # ---------------------------------------------------#
-#   将预测值的每个特征层调成真实值
+#   Adjust each characteristic layer of the predicted value to the true value
 # ---------------------------------------------------#
 def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     num_anchors = len(anchors)
     # [1, 1, 1, num_anchors, 2]
     anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
 
-    # 获得x，y的网格
+    # Get the grid of x and y
     # (13,13, 1, 2)
     grid_shape = K.shape(feats)[1:3]  # height, width
     grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
@@ -130,22 +130,20 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     # (batch_size,13,13,3,85)
     feats = K.reshape(feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
 
-    # 将预测值调成真实值
-    # box_xy对应框的中心点
-    # box_wh对应框的宽和高
+    # Adjust the predicted value to the real value
     box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
     box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
     box_confidence = K.sigmoid(feats[..., 4:5])
     box_class_probs = K.sigmoid(feats[..., 5:])
 
-    # 在计算loss的时候返回如下参数
+    # The following parameters are returned when calculating loss
     if calc_loss is True:
         return grid, feats, box_xy, box_wh
     return box_xy, box_wh, box_confidence, box_class_probs
 
 
 # ---------------------------------------------------#
-#   对box进行调整，使其符合真实图片的样子
+#   Adjust the box so that it looks like the real picture
 # ---------------------------------------------------#
 def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
     box_yx = box_xy[..., ::-1]
@@ -175,17 +173,15 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
 
 
 # ---------------------------------------------------#
-#   获取每个box和它的得分
+#   Get each box and its score
 # ---------------------------------------------------#
 def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
-    # 将预测值调成真实值
-    # box_xy对应框的中心点
-    # box_wh对应框的宽和高
+    # Adjust the predicted value to the real value
     # -1,13,13,3,2; -1,13,13,3,2; -1,13,13,3,1; -1,13,13,3,80
     box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats, anchors, num_classes, input_shape)
-    # 将box_xy、和box_wh调节成y_min,y_max,xmin,xmax
+    # Set box_xy, and box_wh to y_min,y_max,xmin,xmax
     boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
-    # 获得得分和box
+    # Score and box
     boxes = K.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
     box_scores = K.reshape(box_scores, [-1, num_classes])
@@ -193,7 +189,7 @@ def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape)
 
 
 # ---------------------------------------------------#
-#   图片预测
+#   Image prediction
 # ---------------------------------------------------#
 def yolo_eval(yolo_outputs,
               anchors,
@@ -202,23 +198,23 @@ def yolo_eval(yolo_outputs,
               max_boxes=20,
               score_threshold=.6,
               iou_threshold=.5):
-    # 获得特征层的数量
+    # Get the number of feature layers
     num_layers = len(yolo_outputs)
-    # 特征层1对应的anchor是678
-    # 特征层2对应的anchor是345
-    # 特征层3对应的anchor是012
+    # The anchor corresponding to feature layer 1 is 678
+    # The anchor corresponding to feature layer 2 is 345
+    # The anchor corresponding to feature layer 3 is 012
     anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
 
     input_shape = K.shape(yolo_outputs[0])[1:3] * 32
     boxes = []
     box_scores = []
-    # 对每个特征层进行处理
+    # Process each feature layer for processing
     for e in range(num_layers):
         _boxes, _box_scores = yolo_boxes_and_scores(yolo_outputs[e], anchors[anchor_mask[e]], num_classes, input_shape,
                                                     image_shape)
         boxes.append(_boxes)
         box_scores.append(_box_scores)
-    # 将每个特征层的结果进行堆叠
+    # Stack the results for each feature layer
     boxes = K.concatenate(boxes, axis=0)
     box_scores = K.concatenate(box_scores, axis=0)
 
@@ -228,17 +224,15 @@ def yolo_eval(yolo_outputs,
     scores_ = []
     classes_ = []
     for c in range(num_classes):
-        # 取出所有box_scores >= score_threshold的框，和成绩
+        # Retrieve all boxes for box_scores, score_threshold, and scores
         class_boxes = tf.boolean_mask(boxes, mask[:, c])
         class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
 
-        # 非极大抑制，去掉box重合程度高的那一些
+        # remove those with high degree of box overlap
         nms_index = tf.image.non_max_suppression(
             class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
 
-        # 获取非极大抑制后的结果
-        # 下列三个分别是
-        # 框的位置，得分与种类
+
         class_boxes = K.gather(class_boxes, nms_index)
         class_box_scores = K.gather(class_box_scores, nms_index)
         classes = K.ones_like(class_box_scores, 'int32') * c
